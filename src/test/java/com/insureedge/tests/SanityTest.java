f5063bcb1aab9fff6361b3ad83398f877600d651
sarbik sinha <sarbiksinha2002@gmail.com>
2026-02-11 12:40:37 +0530

    added sanity test

---
+++ src/test/java/com/insureedge/tests/SanityTest.java
@@ -0,0 +1,12 @@
+package com.insureedge.tests;
+
+import org.testng.annotations.Test;
+
+public class SanityTest extends BaseUiTest {
+
+    @Test
+    public void checkProfilePage() {
+        System.out.println("sanity test succesfull !!!");
+        // your assertions here
+    }
+}
