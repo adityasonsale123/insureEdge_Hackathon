29dc8d43a84488896da6c43e56021dd18bdf90bd
SounakNeogi <sounakneogi121@gmail.com>
2026-02-13 09:44:18 +0530

    unit test added

---
+++ src/test/java/com/insureedge/tests/UnitTest.java
@@ -0,0 +1,11 @@
+package com.insureedge.tests;
+
+import org.testng.annotations.Test;
+
+public class UnitTest extends  BaseUiTest{
+    @Test
+    public void unitTest(){
+        System.out.println("unit test run!!!");
+    }
+
+}
